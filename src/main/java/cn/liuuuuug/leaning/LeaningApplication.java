2795ad380ca80790776a0b54150410b61eb66a55
liuuuuug <3388941921@qq.com>
2022-06-22 20:43:33 +0800

    Initial commit

---
+++ src/main/java/cn/liuuuuug/leaning/LeaningApplication.java
@@ -0,0 +1,13 @@
+package cn.liuuuuug.leaning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaningApplication.class, args);
+	}
+
+}
